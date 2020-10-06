5c9939b4ed02bb3bcd0477a3edde9a3480ff18cf
Mahesh <maheshrh@hotmail.com>
2020-10-06 22:27:51 +0530

    added homepage

---
+++ precache-manifest.fc80d6e1cd14b61be71ae5e99aeb1129.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "54fd37eb5d912c7b4ebf",
+    "url": "/static/js/main.9a4da5e7.chunk.js"
+  },
+  {
+    "revision": "aa58832b283a409e40ec",
+    "url": "/static/js/2.031270f9.chunk.js"
+  },
+  {
+    "revision": "54fd37eb5d912c7b4ebf",
+    "url": "/static/css/main.be63b08f.chunk.css"
+  },
+  {
+    "revision": "42ea3781c63f8504fe0093457f2c3482",
+    "url": "/index.html"
+  }
+];
